a984c448c9fec3945f00c68d500894e0c2a4a213
KSvetlanA5 <91330943+KSvetlanA5@users.noreply.github.com>
2022-01-10 23:23:14 +0300

    Add files via upload

---
+++ dbo.EngineTypes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EngineTypes] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [Наименование]  NVARCHAR (MAX) NULL,
+    [Мощность] INT            NOT NULL,
+    [Стоимость] INT NOT NULL, 
+    CONSTRAINT [PK_EngineTypes] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
